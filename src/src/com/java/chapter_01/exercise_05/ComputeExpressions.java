5d1a546a238af81d210e7af7b54af31999c38eab
mimiPIllow <erenstonxemin@gmail.com>
2022-05-12 12:24:07 +0300

    chapter 01 (exercise 05)

---
+++ src/src/com/java/chapter_01/exercise_05/ComputeExpressions.java
@@ -0,0 +1,15 @@
+/*
+(Compute expressions) Write a program that displays the result of: (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5)
+*/
+
+package com.java.chapter_01.exercise_05;
+
+public class ComputeExpressions {
+    public static void main(String[] args) {
+        decision();
+    }
+
+    static public void decision() {
+        System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+    }
+}
