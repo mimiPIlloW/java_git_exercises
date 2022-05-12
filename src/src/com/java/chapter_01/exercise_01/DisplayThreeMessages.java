8efa08766a8f3e74cf072086ee988197236868b4
mimiPIllow <erenstonxemin@gmail.com>
2022-05-12 12:22:48 +0300

    chapter 01 (exercise 01)

---
+++ src/src/com/java/chapter_01/exercise_01/DisplayThreeMessages.java
@@ -0,0 +1,18 @@
+/*
+(Display three messages) Write a program that displays Welcome to Java,
+Welcome to Computer Science, and Programming is fun.
+*/
+
+package com.java.chapter_01.exercise_01;
+
+public class DisplayThreeMessages {
+    public static void main(String[] args) {
+        display();
+    }
+
+    static public void display() {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
