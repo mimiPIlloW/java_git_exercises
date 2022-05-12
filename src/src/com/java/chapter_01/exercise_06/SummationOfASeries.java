6498a7b1b052f660c922205d3fd527ce903336b6
mimiPIllow <erenstonxemin@gmail.com>
2022-05-12 12:24:50 +0300

    chapter 01 (exercise 06)

---
+++ src/src/com/java/chapter_01/exercise_06/SummationOfASeries.java
@@ -0,0 +1,16 @@
+/*
+(Summation of a series) Write a program that displays the result of
+				1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9.
+*/
+
+package com.java.chapter_01.exercise_06;
+
+public class SummationOfASeries {
+    public static void main(String[] args) {
+        decision();
+    }
+
+    static public void decision() {
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+    }
+}
