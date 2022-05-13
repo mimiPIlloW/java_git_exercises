5c1bdc5d8c1ec34e8c15fdd2026858dd16aff4c6
mimiPIllow <erenstonxemin@gmail.com>
2022-05-13 16:23:54 +0300

    chapter 01 (exercise 09)

---
+++ src/src/com/java/chapter_01/exercise_09/AreaAndPerimeterOfARectangle.java
@@ -0,0 +1,18 @@
+/*
+(Area and perimeter of a rectangle) Write a program that displays the area and
+perimeter of a rectangle with the width of 4.5 and height of 7.9 using the following
+formula: area = width * height
+*/
+
+package com.java.chapter_01.exercise_09;
+
+public class AreaAndPerimeterOfARectangle {
+    public static void main(String[] args) {
+        double width = 4.5;
+        double height = 7.9;
+
+        double area = width * height;
+
+        System.out.println("Area =" + area);
+    }
+}
